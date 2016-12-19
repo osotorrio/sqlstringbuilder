a9dc09f426e72bc3fcfc18dffbd854737222f49b
osotorrio <osotorrio@hotmail.com>
2016-12-19 11:46:51 +0000

    Added missing contract

--- SqlStringBuilder.Core/ICUDCommands.cs
+++ SqlStringBuilder.Core/ICUDCommands.cs
@@ -3,7 +3,11 @@
     public interface ICUDCommands
     {
         SqlSb InsertInto(string tableName);
+
         SqlSb Values(string valuesSeparatedByComa);
+
         SqlSb Delete();
+
+        SqlSb Update(string tableName);
     }
 }
